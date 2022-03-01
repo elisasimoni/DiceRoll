f0fb9dce46448c0b553b534fb19242d6f338961c
elisa <elisa.simoni4@studio.unibo.it>
2022-03-01 15:30:46 +0100

    add image

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 /**
@@ -14,13 +15,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
+        val diceImage: ImageView = findViewById(R.id.dice)
+        diceImage.setImageResource(R.drawable.dice_1)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
-            val resultTextView: TextView = findViewById(R.id.textView)
-
             val dice = Dice(6)
-            resultTextView.text = dice.roll().toString()
+            val rollResult = dice.roll()
+
+            when (rollResult) {
+
+                1 -> diceImage.setImageResource(R.drawable.dice_1)
+                2 -> diceImage.setImageResource(R.drawable.dice_2)
+                3 -> diceImage.setImageResource(R.drawable.dice_3)
+                4 -> diceImage.setImageResource(R.drawable.dice_4)
+                5 -> diceImage.setImageResource(R.drawable.dice_5)
+                6 -> diceImage.setImageResource(R.drawable.dice_6)
+            }
+
+
+
+
+
         }
     }
 }
